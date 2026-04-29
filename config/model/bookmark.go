89f1f74402309b63cd216507ed38bba04e95aa6d
vickai <i@vickai.com>
2026-04-29 10:56:25 +0800

    新增服务在线检测

--- config/model/bookmark.go
+++ config/model/bookmark.go
@@ -21,3 +21,20 @@ type Bookmarks struct {
 	Categories []Category `yaml:"categories,omitempty"`
 	Items      []Bookmark `yaml:"links"`
 }
+
+// 支持 IP 探测
+type VickaiBookmark struct {
+	Name     string `yaml:"name"`
+	URL      string `yaml:"link"`
+	Icon     string `yaml:"icon,omitempty"`
+	Desc     string `yaml:"desc,omitempty"`
+	IP   	 string `yaml:"ip,omitempty"`
+	Port     int    `yaml:"port,omitempty"`
+}
+
+// 支持 IP 端口服务在线探测
+type VickaiService struct {
+	Name     string `yaml:"name"`
+	IP   	 string `yaml:"ip,omitempty"`
+	Port     int    `yaml:"port,omitempty"`
+}
